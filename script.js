c00b91bde908dd23618202bed9488b801a59753e
Seshanshree M <seshanshreem@gmail.com>
2026-04-30 22:07:11 +0530

    Create responsive 6-page IMAX portfolio template

---
+++ script.js
@@ -0,0 +1,4 @@
+const path = window.location.pathname.split('/').pop() || 'index.html';
+document.querySelectorAll('.nav-links a').forEach(link => {
+  if (link.getAttribute('href') === path) link.classList.add('active');
+});
